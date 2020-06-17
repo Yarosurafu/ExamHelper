07ca654aceed67c348f84ed9217fbebdf9d41d06
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-17 17:59:00 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,13 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "parsingdb.h"
+#include "parsingprogress.h"
 
 #include <QFileDialog>
 #include <QDir>
 #include <QFileInfoList>
+#include <chrono>
+#include <thread>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -20,13 +23,23 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_parseButt_clicked()
 {
+    static ParsingProgress progress(this);
+    std::chrono::seconds dura(5);
     QString dirName = ui->directoryLine->text();
     ui->directoryLine->clear();
     QDir directory(dirName);
     QFileInfoList files = directory.entryInfoList();
+    progress.setProgressMin(0);
+    progress.setProgressMax(files.size());
+    progress.show();
     for(int i = 0; i < files.size(); ++i){
-
+        progress.setProgress(i);
+        if(files[i].absoluteFilePath().contains(".html"))
+            ParsingDB::parse(files[i].absoluteFilePath());
+        std::this_thread::sleep_for(dura);
     }
+    progress.hide();
+    progress.resetProgress();
 }
 
 void MainWindow::on_pickFileButt_clicked()
