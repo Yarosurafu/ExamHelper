018ea9e0b0065cd90a5ae5f392ae0a2eb5fda3c3
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-21 22:56:11 +0300

    Create database.h

---
+++ database.h
@@ -0,0 +1,13 @@
+﻿#ifndef DATABASE_H
+#define DATABASE_H
+#include <QJsonArray>
+
+class DataBase
+{
+    QJsonArray subjects_m;
+public:
+    DataBase();
+    QString* getAllSubjects(const int* size);
+};
+
+#endif // DATABASE_H
