f13f5f8e399c2d3f236317d07ca5400f4eb0b738
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-21 22:56:09 +0300

    Create database.cpp

---
+++ database.cpp
@@ -0,0 +1,21 @@
+﻿#include "database.h"
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFile>
+#include <QApplication>
+#include <QJsonObject>
+
+DataBase::DataBase()
+{
+    QString filePath = QApplication::applicationDirPath();
+    QFile loadFile(filePath + "/database/test.json");
+    if(!loadFile.open(QIODevice::ReadOnly)){
+        qWarning("Error opening JSON-file");
+        return;
+    }
+    QByteArray data = loadFile.readAll();
+    loadFile.close();
+    QJsonDocument db(QJsonDocument::fromJson(data));
+    QJsonObject buff = db.object();
+    subjects_m = buff["questions"].toArray();
+}
