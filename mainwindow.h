51cb047415459a018e8963ca8000195262a5ac71
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-22 21:05:13 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "database.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -22,5 +23,6 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    DataBase db_m;
 };
 #endif // MAINWINDOW_H
