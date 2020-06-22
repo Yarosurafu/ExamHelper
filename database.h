98f7ac2b30f5156220fa1571d65e4b3134562564
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-22 21:04:53 +0300

    Update database.h

--- database.h
+++ database.h
@@ -1,13 +1,19 @@
 ﻿#ifndef DATABASE_H
 #define DATABASE_H
 #include <QJsonArray>
+#include <vector>
+#include <QString>
 
 class DataBase
 {
     QJsonArray subjects_m;
+    std::vector<QString> subjectsNames_m;
+
 public:
     DataBase();
-    QString* getAllSubjects(const int* size);
+    std::vector<QString> getSubjectsNames(){
+        return subjectsNames_m;
+    }
 };
 
 #endif // DATABASE_H
