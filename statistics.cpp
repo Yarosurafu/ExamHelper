49e741aa7334ae3164b9edb1a99fde2e7320041a
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-06-30 20:59:08 +0300

    Update statistics.cpp

--- statistics.cpp
+++ statistics.cpp
@@ -14,14 +14,14 @@ Statistics::Statistics(int correctAnswQuant, QWidget *parent) :
     QPen whitePen(Qt::white);
     QBrush redBrush(Qt::red);
     QBrush whiteBrush(Qt::white);
-    ellipse = scene->addEllipse(-30, -36, 100, 100, whitePen, redBrush);
+    ellipse = scene->addEllipse(-35, -37, 100, 100, whitePen, redBrush);
     ellipse->setSpanAngle(angle);
     ellipse->setStartAngle(1440);
-    QGraphicsEllipseItem* center = scene->addEllipse(-5, -11, 50, 50, whitePen, whiteBrush);
+    QGraphicsEllipseItem* center = scene->addEllipse(-10, -12, 50, 50, whitePen, whiteBrush);
     center->setSpanAngle(angle);
     center->setStartAngle(1440);
     scene->setFocusItem(ellipse);
-    scene->addText(QString::number(correctAnswQuant) + "/30");
+    scene->addText(QString::number(correctAnswQuant * 100 / 30) + "%");
     ui->statView->setScene(scene);
 }
 
