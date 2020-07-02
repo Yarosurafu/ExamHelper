0fab0aed78c662231dc17667436e49d76df79776
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-07-02 20:14:39 +0300

    Update statistics.cpp

--- statistics.cpp
+++ statistics.cpp
@@ -3,29 +3,44 @@
 #include <QGraphicsScene>
 #include <QGraphicsEllipseItem>
 
-Statistics::Statistics(int correctAnswQuant, QWidget *parent) :
+Statistics::Statistics(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Statistics)
 {
-    int angle = correctAnswQuant * 360 * 16 / 30;
     ui->setupUi(this);
     scene = new QGraphicsScene(this);
+}
+
+void Statistics::setData(const int correctAnswers){
+
+    int angle = correctAnswers * 360 * 16 / 30;
     QPen blackPen(Qt::black);
     QPen whitePen(Qt::white);
     QBrush redBrush(Qt::red);
     QBrush whiteBrush(Qt::white);
-    ellipse = scene->addEllipse(-35, -37, 100, 100, whitePen, redBrush);
+    ellipse = new QGraphicsEllipseItem(-35, -37, 100, 100);
+    ellipse->setPen(whitePen);
+    ellipse->setBrush(redBrush);
     ellipse->setSpanAngle(angle);
     ellipse->setStartAngle(1440);
-    QGraphicsEllipseItem* center = scene->addEllipse(-10, -12, 50, 50, whitePen, whiteBrush);
+    scene->addItem(ellipse);
+    center = new QGraphicsEllipseItem(-10, -12, 50, 50);
+    center->setPen(whitePen);
+    center->setBrush(whiteBrush);
     center->setSpanAngle(angle);
     center->setStartAngle(1440);
-    scene->setFocusItem(ellipse);
-    scene->addText(QString::number(correctAnswQuant * 100 / 30) + "%");
+    scene->addItem(center);
+    scene->addText(QString::number(correctAnswers * 100 / 30) + "%");
     ui->statView->setScene(scene);
+    ui->correctQuant->setText(QString::number(correctAnswers));
+    ui->incorrectQuant->setText(QString::number(30 - correctAnswers));
+    ui->statView->repaint();
 }
 
 Statistics::~Statistics()
 {
+    delete ellipse;
+    delete center;
+    delete scene;
     delete ui;
 }
