17049e95ee88970b71cb66b269cf0c9ca89f6a64
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-07-02 23:50:00 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -2,6 +2,7 @@
 #include "ui_tests.h"
 #include <QJsonObject>
 #include <QJsonArray>
+#include <QMessageBox>
 
 Tests::Tests(const QJsonArray questions, Statistics* statWindwow, QWidget *parent) :
     QWidget(parent),
@@ -12,11 +13,34 @@ Tests::Tests(const QJsonArray questions, Statistics* statWindwow, QWidget *paren
     ui->setupUi(this);
     setQuestion();
     connect(ui->nextQuestBut, SIGNAL(clicked()), this, SLOT(setQuestion()));
+
+    connect(ui->firstAnsw, SIGNAL(clicked()), this, SLOT(checkAnswer()));
+    connect(ui->secondAnsw, SIGNAL(clicked()), this, SLOT(checkAnswer()));
+    connect(ui->thirdAnsw, SIGNAL(clicked()), this, SLOT(checkAnswer()));
+    connect(ui->fourthAnsw, SIGNAL(clicked()), this, SLOT(checkAnswer()));
+    connect(ui->fifthAnsw, SIGNAL(clicked()), this, SLOT(checkAnswer()));
+    answerButtons[0] = ui->firstAnsw;
+    answerButtons[1] = ui->secondAnsw;
+    answerButtons[2] = ui->thirdAnsw;
+    answerButtons[3] = ui->fourthAnsw;
+    answerButtons[4] = ui->fifthAnsw;
+    for(int i = 0; i < 5; ++i)
+        answerButtons[i]->setCheckable(true);
+    answerButtons[0]->setChecked(true);
 }
 
 Tests::~Tests()
 {
     delete ui;
+    for(int i = 0; i < 5; ++i)
+        delete answerButtons[i];
+}
+
+bool Tests::checkButtons(){
+    for(int i = 0; i < 5; ++i)
+        if(answerButtons[i]->isChecked())
+            return true;
+    return false;
 }
 
 void Tests::setQuestion(){
@@ -25,25 +49,57 @@ void Tests::setQuestion(){
         ui->progressBar->setValue(currentQuestion);
     }
     else{
-        statWindow->setData(correctQuestions);
+        statWindow->setData(correctAnswers);
         emit testEnd();
         return;
     }
+    if(!checkButtons()){
+        QMessageBox::critical(this, "Ошибка", "Ответ не был выбран");
+        return;
+    }
+
+    isAnswered = false;
     //Changing buttons theme to "base"
-    ui->firstAnsw->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
-    ui->secondAnsw->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
-    ui->thirdAnsw->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
-    ui->fourthAnsw->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
-    ui->fifthAnsw->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
+    for(int i = 0; i < 5; ++i){
+        answerButtons[i]->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.884615, y2:0.892, stop:0 rgba(85, 170, 255, 255), stop:1 rgba(255, 255, 255, 255));\n	border: 2px solid rgb(0, 85, 255);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
+        answerButtons[i]->setChecked(false);
+    }
     //Changing question and answers
     QJsonObject question = questions[currentQuestion].toObject();
     ui->questionText->setText(question["question"].toString());
     QJsonArray answers = question["answers"].toArray();
-    ui->firstAnsw->setText(answers[0].toString());
-    ui->secondAnsw->setText(answers[1].toString());
-    ui->thirdAnsw->setText(answers[2].toString());
-    ui->fourthAnsw->setText(answers[3].toString());
-    ui->fifthAnsw->setText(answers[4].toString());
+    //TODO: DELETE THE '*'!!!
+    for(int i = 0; i < answers.size(); ++i){
+        answerButtons[i]->setText(answers[i].toString());
+        if(answers[i].toString().contains("*"))
+            answerButtons[i]->setChecked(true);
+    }
     if(currentQuestion == 29)
         ui->nextQuestBut->setText("Завершить тест");
+}
+
+void Tests::checkAnswer(){
+    if(isAnswered)
+        return;
+    QPushButton *userAnswButt = static_cast<QPushButton*>(sender());
+    QString userAnsw = userAnswButt->text();
+    QString currCorrectAnsw;
+
+    for(int i = 0; i < 5; ++i)
+        if(answerButtons[i]->isChecked()){
+            answerButtons[i]->setStyleSheet("QPushButton{	\n	color: rgb(0, 85, 0);\n	background-color: rgb(85, 255, 127);\n	border: 4px solid rgb(0, 170, 0);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
+            currCorrectAnsw = answerButtons[i]->text();
+            break;
+        }
+
+    if(userAnsw == currCorrectAnsw){
+        ++correctAnswers;
+        isAnswered = true;
+        return;
+    }
+    else{
+        userAnswButt->setStyleSheet("QPushButton{	\n	color: black;\n	background-color: rgb(255, 93, 93);\n	border: 4px solid rgb(170, 0, 0);\n	padding: 6px;\n	border-radius: 20px 10px;\n}\n\nQPushButton:pressed{\n	color: red;\n	border: 4px solid red;\n	border-radius: 20px 10px;\n}");
+        isAnswered = true;
+    }
+
 }
