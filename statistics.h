109976e0c01f9dfe565615f40c6adf51df73e860
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-07-07 19:23:33 +0300

    Update statistics.h

--- statistics.h
+++ statistics.h
@@ -23,6 +23,12 @@ private:
     QGraphicsScene *scene;
     QGraphicsEllipseItem *ellipse = nullptr;
     QGraphicsEllipseItem *center = nullptr;
+signals:
+    void closeAll();
+    void repeat();
+private slots:
+    void on_closeButton_clicked();
+    void on_repeatButton_clicked();
 };
 
 #endif // STATISTICS_H
