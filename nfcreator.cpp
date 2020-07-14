b4e7534dc9d414c05d6eb9dbe2fbe9eda34fda5b
Yarosurafu <58003887+Yarosurafu@users.noreply.github.com>
2020-07-14 18:59:50 +0300

    Update nfcreator.cpp

--- nfcreator.cpp
+++ nfcreator.cpp
@@ -1,5 +1,12 @@
 ﻿#include "nfcreator.h"
 #include "ui_nfcreator.h"
+#include <QDebug>
+#include <QMessageBox>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QTextCodec>
 
 NfCreator::NfCreator(QWidget *parent) :
     QWidget(parent),
@@ -14,10 +21,53 @@ NfCreator::~NfCreator()
 }
 
 void NfCreator::setQuestion(QString questionArg){
-    question = questionArg;
+    m_question = questionArg;
+    qDebug() << m_question << "\n";
 }
 
-void NfCreator::on_pushButton_5_clicked()
+void NfCreator::on_cancelBut_clicked()
 {
-    this->close();
+    emit exit();
+}
+
+void NfCreator::on_createNotifBut_clicked()
+{
+    QDate chosenDate = ui->calendar->selectedDate();
+    int year = chosenDate.year();
+    int month = chosenDate.month();
+    int day = chosenDate.day();
+    m_date = "/sd " + QString::number(day) + "/" + QString::number(month) + "/"
+            + QString::number(year) + " /st " + ui->timeEdit->text() + " /st once";
+    writeToJson();
+    emit exit();
+}
+
+void NfCreator::writeToJson(){
+    QString path = QApplication::applicationDirPath()+ "/database/notifications.json";
+    qDebug() << path;
+    QFile file(path);
+    if(!file.open(QIODevice::ReadOnly)){
+        qWarning("Cannot open JSON-file");
+        return;
+    }
+    QByteArray data = file.readAll();
+    file.close();
+    QJsonDocument document(QJsonDocument::fromJson(data));
+    QJsonObject database = document.object();
+    QJsonArray notifications = database["notifications"].toArray();
+
+    QJsonObject newNotif = {
+      {"date", m_date},
+      {"question", m_question}
+    };
+
+    notifications.append(newNotif);
+    database["notifications"] = notifications;
+    QFile saveFile(path);
+    if(!saveFile.open(QIODevice::WriteOnly | QIODevice::Text)){
+        qWarning("Cannot open save JSON");
+        return;
+    }
+    saveFile.write(QJsonDocument(database).toJson());
+    saveFile.close();
 }
