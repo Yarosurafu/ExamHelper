cfe4e6913511baca1b18f14c22a00bc7ce088e7a
Yarosurafu <yaroslavsvirin@gmail.com>
2020-08-08 13:42:01 +0300

    Added Basya to resource

---
+++ notification.cpp
@@ -0,0 +1,6 @@
+#include "notification.h"
+
+Notification::Notification()
+{
+
+}
---
+++ notification.h
@@ -0,0 +1,11 @@
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+
+class Notification
+{
+public:
+    Notification();
+};
+
+#endif // NOTIFICATION_H
