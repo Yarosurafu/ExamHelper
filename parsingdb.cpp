dda44e9b2ba2669165d9252588835ad69de053d1
Yarosurafu <yaroslavsvirin@gmail.com>
2020-08-22 22:06:20 +0300

    Added methods for parsing bookletes

--- parsingdb.cpp
+++ parsingdb.cpp
@@ -24,6 +24,148 @@ ParsingDB::ParsingDB()
  */
 void ParsingDB::parse(const QString path){
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
+    //----------Opening HTML-file----------
+    QFile html(path);
+    if(!html.open(QIODevice::ReadOnly)){
+        qWarning("Cannot open HTML-file");
+        return;
+    }
+    QTextStream in(&html);
+    //-------------------------------------
+    for(int i = 0; i < 5 && !in.atEnd(); ++i){
+        QString line = in.readLine();
+        if(line.contains("<title>КРОК")){
+            parseBooklet(path);
+            return;
+        }
+    }
+    parseTests(path);
+}
+
+void ParsingDB::parseBooklet(const QString path){
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
+    //----------Opening JSON-file----------
+    QString filePath = QApplication::applicationDirPath();
+    QFile loadFile(filePath + "/database/test.json");
+    if(!loadFile.open(QIODevice::ReadOnly)){
+        qWarning("Cannot open JSON-file");
+        return;
+    }
+    QByteArray data = loadFile.readAll();
+    QJsonDocument doc(QJsonDocument::fromJson(data));
+    QJsonObject database = doc.object();
+    QJsonArray questions = database["questions"].toArray();
+    loadFile.close();
+    //-------------------------------------
+
+    //----------Opening HTML-file----------
+    QFile html(path);
+    if(!html.open(QIODevice::ReadOnly)){
+        qWarning("Cannot open HTML-file");
+        return;
+    }
+    QTextStream in(&html);
+    //-------------------------------------
+
+    //-------------Parsing-----------------
+    while(!in.atEnd()){
+        QString line = in.readLine();
+        //Checking subject and subject matter
+        if(line.contains("Буклет")){
+            while(line.at(0) == ' ')
+                line.remove(0, 1);
+            //Now line is a current subject matter
+            QJsonObject currMatter;
+            QJsonArray currMatterQuestions;
+            int matterInd = indexOfElement(questions, line);
+            //If DB already has current subject matter
+            if(matterInd >= 0){
+                currMatter = questions[matterInd].toObject();
+                currMatterQuestions = currMatter["questions"].toArray();
+            }
+            for(int i = 0; i < 50; ++i){
+                //-----Getting question-----
+                QString question;
+                do{
+                    question = in.readLine();
+                }while(!question.contains("<div class=\"col-xs-6\">") && !in.atEnd());
+                if(in.atEnd())
+                    break;
+                question.remove("<div class=\"col-xs-6\">");
+                question.remove("<span class=\"hidden-xs\">");
+                question.remove("</span>");
+                question.remove("<b>");
+                question.remove("</b></div>");
+                while(question.at(0) == ' ')
+                    question.remove(0, 1);
+                QString lastPart;
+                for(int i = 0; i < 6; ++i)
+                    lastPart = in.readLine();
+                lastPart.remove("<div class=\"krt_question\">");
+                lastPart.remove("</div>");
+                while(lastPart.at(0) == ' ')
+                    lastPart.remove(0, 1);
+                question += " " + lastPart;
+                //--------------------------
+
+                //------Getting answers-----
+                QJsonArray answers;
+                for(int i = 0; i < 5; ++i){
+                    QString answer;
+                    do{
+                        answer = in.readLine();
+                        answer.remove("\t");
+                    } while(!answer.contains("<li class=\"list-group-item") && !in.atEnd());
+
+                    //If answer is correct
+                    if(answer.contains("list-group-item-success")){
+                        answer.remove("<li class=\"list-group-item  list-group-item-success\" style=\"border:none;\">");
+                        answer.remove("</li>");
+                        answer.append("*");
+                    }
+                    else{
+                        answer.remove("<li class=\"list-group-item \" style=\"border:none;\">");
+                        answer.remove("</li>");
+                    }
+                    while(answer.at(0) == ' ')
+                        answer.remove(0, 1);
+                    answers.append(answer);
+                }
+                //--------------------------
+
+                //Creating question with the answers
+                QJsonObject fullQuestion = {
+                    {"question", question},
+                    {"answers", answers}
+                };
+                currMatterQuestions.append(fullQuestion);
+            }
+            //Saving
+            QFile saveFile(filePath + "/database/test.json");
+            if(!saveFile.open(QIODevice::WriteOnly | QIODevice::Text)){
+                qWarning("Cannot open save JSON");
+                return;
+            }
+            QJsonObject newSubject = {
+                {"subject", line},
+                {"questions", currMatterQuestions}
+            };
+            if(matterInd >= 0){
+                questions[matterInd] = newSubject;
+            }
+            else{
+                questions.append(newSubject);
+            }
+            database["questions"] = questions;
+            saveFile.write(QJsonDocument(database).toJson());
+            saveFile.close();
+        }
+    }
+    //-------------------------------------
+}
+
+void ParsingDB::parseTests(const QString path){
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
     //----------Opening JSON-file----------
     QString filePath = QApplication::applicationDirPath();
     QFile loadFile(filePath + "/database/test.json");
